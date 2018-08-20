ace61c730b41e670f90b3bfe1fb67840f3b94095
Thiago de Almeida Santos <thiagocotrim1@gmail.com>
2018-08-19 22:49:09 -0300

    Primeira versão da aplicação de negociacoes

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,76 @@
+class NegociacaoController {
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
+        );
+
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($("#mensagemView")),
+            'texto'
+        );
+    }
+
+    adciona(event) {
+        event.preventDefault();
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        //this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adcionada com sucesso!';
+        //this._mensagemView.update(this._mensagem);
+
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    importaNegociacoes() {
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((err, negociacoes) => {
+            if (err) {
+                this._mensagem.texto = err;
+                return;
+            }
+
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        })
+
+
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = '1';
+        this._inputValor.value = '0.0';
+
+        this._inputData.focus();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        // this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso!';
+        //this._mensagemView.update(this._mensagem);
+    }
+}
